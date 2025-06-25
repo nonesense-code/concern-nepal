7e3306272c974e8556402ec21310c828210ad5ca
nonesense-code <kaushalkadariya2062@gmail.com>
2025-06-25 22:50:34 +0545

    changes done

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,3 +1,7 @@
+import { FaFacebook } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <>
@@ -5,12 +9,19 @@ const Footer = () => {
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
             <div>
-              <h2>Concern</h2>
+              <h2 className="text-lg">Concern</h2>
               <p>
                 Empowering change through research and data-driven insights
                 across Nepal.
               </p>
             </div>
+            <ul>
+              <li className="flex items-center justify-center gap-2">
+                <FaFacebook />
+                <FaTwitter />
+                <FaLinkedin />
+              </li>
+            </ul>
             <div></div>
             <div></div>
           </div>
